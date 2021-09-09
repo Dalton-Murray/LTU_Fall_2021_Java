dbaf1d3d4edf90980f3a2547e9f9e61bb5df3b0a
Dalton-Murray <murraydalton1@comcast.net>
2021-09-08 22:21:08 -0400

    Chapter 8

---
+++ Chapter 8/8.6_DateParser.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class DateParser {
+    public static int getMonthAsInt(String monthString) {
+        int monthInt;
+
+        switch (monthString) {
+        case "January":
+            monthInt = 1;
+            break;
+        case "February":
+            monthInt = 2;
+            break;
+        case "March":
+            monthInt = 3;
+            break;
+        case "April":
+            monthInt = 4;
+            break;
+        case "May":
+            monthInt = 5;
+            break;
+        case "June":
+            monthInt = 6;
+            break;
+        case "July":
+            monthInt = 7;
+            break;
+        case "August":
+            monthInt = 8;
+            break;
+        case "September":
+            monthInt = 9;
+            break;
+        case "October":
+            monthInt = 10;
+            break;
+        case "November":
+            monthInt = 11;
+            break;
+        case "December":
+            monthInt = 12;
+            break;
+        default:
+            monthInt = 00;
+        }
+
+        return monthInt;
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        String date;
+        int index, month_int;
+        String month, day, year;
+
+        while (true) {
+            date = scnr.nextLine();
+
+            if (date.equals("-1") == true) {
+                break;
+            }
+
+            index = date.indexOf(",");
+            if (index == -1) {
+                continue;
+            }
+
+            String month_date = date.substring(0, index);
+            month = month_date.split(" ")[0];
+            day = month_date.split(" ")[1];
+            year = date.substring(index + 2);
+
+            month_int = getMonthAsInt(month);
+
+            System.out.println(month_int + "/" + day + "/" + year);
+
+        }
+
+    }
+}
---
+++ Chapter 8/8.7_File name change.java
@@ -0,0 +1,63 @@
+/*
+
+Dalton Murray
+09/08/2021
+
+*/
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LabProgram {
+    public static void main(String[] args) {
+        String fileName;
+        String tempFileName;
+        Scanner scnr = new Scanner(System.in);
+
+        fileName = scnr.nextLine();
+        tempFileName = "image" + fileName;
+
+        BufferedReader br = null;
+        BufferedWriter bw = null;
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            bw = new BufferedWriter(new FileWriter(tempFileName));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.contains("_photo.jpg")) {
+                    line = line.replace("_photo.jpg", "_info.txt");
+                }
+                bw.write(line+"\n");
+
+                System.out.println(line);
+            }
+        }
+        catch (Exception e) {
+            return;
+        }
+        finally {
+            try {
+                if(br != null)
+                    br.close();
+            }
+            catch (IOException e) {
+            }
+            try {
+                if(bw != null)
+                    bw.close();
+            }
+            catch (IOException e) {
+            }
+        }
+
+        File oldFile = new File(fileName);
+        oldFile.delete();
+
+        File newFile = new File(tempFileName);
+        newFile.renameTo(oldFile);
+    }
+}
