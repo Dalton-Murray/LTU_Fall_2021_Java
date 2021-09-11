8b31aff9adee63e7d35f7a94ff943ff74fde830d
Dalton-Murray <murraydalton1@comcast.net>
2021-09-11 19:43:14 -0400

    Chapter 10

---
+++ Chapter 10/10.8_Simple integer division - multiple exception handlers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      try {
+         int userNumber = scnr.nextInt();
+         int divisionNumber  = scnr.nextInt();
+            
+         System.out.println(userNumber / divisionNumber);
+      }
+      catch (ArithmeticException e) {
+         System.out.println("Arithmetic Exception: " + e.getMessage());
+      }
+      catch (InputMismatchException e) {
+         System.out.println("Input Mismatch Exception: " + e.toString());
+      }
+   }
+}
